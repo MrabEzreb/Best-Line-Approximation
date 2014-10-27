9182df1139551298c8e996a49ac5c35c14f6b60e
MrabEzreb <zerbebram@gmail.com>
2014-10-27 10:49:21 -0400

    Slope Intercept Form
    Added a class for figureing out slope intercept form.

--- com/ezreb/math/BestLineApprox.java
+++ com/ezreb/math/BestLineApprox.java
@@ -1,10 +1,15 @@
 package com.ezreb.math;
 
+import java.util.Scanner;
+
 public class BestLineApprox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner reader = new Scanner(new FileInputStream("Best-Line-Points.txt");
+		Object ex;
+		int n = ex.length
+		for i = 1, n
 
 	}
 
---
+++ com/ezreb/math/SlopeIntercept.java
@@ -0,0 +1,10 @@
+package com.ezreb.math;
+
+public class SlopeIntercept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
