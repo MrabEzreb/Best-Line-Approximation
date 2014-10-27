654c494faba1229eb588a6deb8f2186bba83014f
MrabEzreb <zerbebram@gmail.com>
2014-10-27 11:08:51 -0400

    Working on it!

--- com/ezreb/math/SlopeIntercept.java
+++ com/ezreb/math/SlopeIntercept.java
@@ -3,8 +3,22 @@ package com.ezreb.math;
 public class SlopeIntercept {
 
 	public static void main(String[] args) {
+		double x1 = 0;
+		double y1 = 0;
 		// TODO Auto-generated method stub
-
+		XYPoint p1 = new XYPoint(x1, y1);
+		double x2 = 0;
+		double y2 = 0;
+		XYPoint p2 = new XYPoint(x2, y2);
+		double rise = p2.y-p1.y;
+		double run = p2.x-p1.x;
+		double slope = rise/run;
+		if(p1.x < 0) {
+			p1.x = p1.x*-1;
+			slope = slope*-1;
+		}
+		
+			
 	}
 
 }
---
+++ com/ezreb/math/XYPoint.java
@@ -0,0 +1,11 @@
+package com.ezreb.math;
+
+public class XYPoint {
+	public XYPoint(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	double x;
+	double y;
+	
+}
