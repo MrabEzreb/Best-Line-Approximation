b67b2eb8bc774e0771ae268150e31a5718e6dd07
MrabEzreb <zerbebram@gmail.com>
2014-10-29 07:48:00 -0400

    Errors!
    I fixed a few errors, however, I still don't know how to read the .txt
    file!

--- com/ezreb/math/BestLineApprox.java
+++ com/ezreb/math/BestLineApprox.java
@@ -1,15 +1,18 @@
 package com.ezreb.math;
 
+import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class BestLineApprox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner reader = new Scanner(new FileInputStream("Best-Line-Points.txt");
-		Object ex;
-		int n = ex.length
-		for i = 1, n
+		Scanner reader = new Scanner(new FileInputStream("/Best-Line-Approximation/com/ezreb/math/Best-Line-Points"));
+		String[] ex = null;
+		int n = ex.length;
+		int i;
+		for(i=1; i<n; i = i+1);
+			
 
 	}
 
