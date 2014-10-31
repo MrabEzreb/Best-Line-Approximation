9f674d2ccc8a6bfe8276c2074f99ce2b89b0b3f3
MrabEzreb <zerbebram@gmail.com>
2014-10-31 11:02:29 -0400

    v0.1.3
    Made the window even prettier, also, I have started a new versioning
    scheme which can be found at
    https://github.com/MrabEzreb/BestLineApproximation/wiki/Versioning

--- com/ezreb/math/MathWindow.java
+++ com/ezreb/math/MathWindow.java
@@ -1,10 +1,14 @@
 package com.ezreb.math;
 
 import java.awt.Button;
-import java.awt.Choice;
+import java.awt.Dimension;
+//import java.awt.Choice;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Label;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,15 +23,22 @@ public class MathWindow implements ActionListener {
 		windo2.setVisible(true);
 	    btn1.setActionCommand("tf1.setText('I Did It!'");
 	    tf1.setText("This is a textbox");
-	    chooser.add("Pick an Option");
-	    chooser.add("Slope Intercept Form");
-	    chooser.add("Test1");
-	    chooser.add("Test 2ledelley do");
-	    windo2.add(lblOne);
-	    windo2.add(chooser);
 	    windo2.add(btn1);
 	    windo2.add(tf1);
-	    windo2.setLayout(new FlowLayout(FlowLayout.CENTER));
+	    Dimension tf1s = new Dimension();
+	    tf1s.setSize(145, 70);
+	    tf1.setPreferredSize(tf1s);
+//	    chooser.add("Pick an Option");
+//	    chooser.add("Slope Intercept Form");
+//	    chooser.add("Test1");
+//	    chooser.add("Test 2ledelley do");
+//	    windo2.add(lblOne);
+//	    windo2.add(chooser);
+	    Dimension btn1s = new Dimension();
+	    btn1s.setSize(145, 30);
+	    btn1.setPreferredSize(btn1s);
+	    windo2.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+//	    windo2.setLayout(new FlowLayout(FlowLayout.CENTER));
 	    
 	}
 	Label lblOne = new Label("Do Math Here");
@@ -37,7 +48,7 @@ public class MathWindow implements ActionListener {
 	}
 	
 	Button btn1 = new Button("Compute");
-	Choice chooser = new Choice();
+//	Choice chooser = new Choice();
 	boolean isActive = false;
 	TextField tf1 = new TextField();
 	public void stop() {
@@ -45,7 +56,7 @@ public class MathWindow implements ActionListener {
 	}
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("Button Pressed");
-		String chosenMath = chooser.getSelectedItem();
+//		String chosenMath = chooser.getSelectedItem();
 		if(chosenMath=="Slope Intercept Form") {
 			System.out.println("SlopeInterceptCHosen");
 			String values = tf1.getText();
@@ -66,8 +77,36 @@ public class MathWindow implements ActionListener {
 			MathWindow.this.close();
 		}
 	};
+	MenuBar menuBar = new MenuBar();
+	Menu menuView = new Menu("View");
+	MenuItem top = new MenuItem("Always On Top");
+	Menu menuCalc = new Menu("Calculation");
+	MenuItem c1 = new MenuItem("Slope-Intercept Form");
+	String chosenMath = null;
 	public void start() throws InterruptedException {
 		btn1.addActionListener(this);
 		windo2.addWindowListener(l);
+		top.addActionListener(new ActionListener() {
+			boolean isTop = false;
+			public void actionPerformed(ActionEvent e) {
+				if(isTop==false) {
+					windo2.setAlwaysOnTop(true);
+					isTop = true;
+				} else {
+					windo2.setAlwaysOnTop(false);
+					isTop = false;
+				}
+			}
+		});
+		c1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				chosenMath = "Slope Intercept Form";
+			}
+		});
+		menuView.add(top);
+		menuBar.add(menuView);
+		windo2.setMenuBar(menuBar);
+		menuCalc.add(c1);
+		menuBar.add(menuCalc);
 	}
 }
