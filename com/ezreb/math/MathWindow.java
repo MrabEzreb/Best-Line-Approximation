1fd6d5e85adfc148e028ac62b8d3be20a4337073
MrabEzreb <zerbebram@gmail.com>
2014-10-31 09:49:58 -0400

    v0.0.2
    FIRST FULL RELEASE!
    Changelog:
    Made window smaller
    made it possible to close the window normally!

--- com/ezreb/math/MathWindow.java
+++ com/ezreb/math/MathWindow.java
@@ -8,12 +8,14 @@ import java.awt.Label;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Scanner;
 
 public class MathWindow implements ActionListener {
 	public MathWindow() {
 		windo2.setResizable(false);
-		windo2.setSize(800, 450);
+		windo2.setSize(150, 150);
 		windo2.setVisible(true);
 	    btn1.setActionCommand("tf1.setText('I Did It!'");
 	    tf1.setText("This is a textbox");
@@ -21,21 +23,19 @@ public class MathWindow implements ActionListener {
 	    chooser.add("Slope Intercept Form");
 	    chooser.add("Test1");
 	    chooser.add("Test 2ledelley do");
-	    chooser.add("End Program");
 	    windo2.add(lblOne);
+	    windo2.add(chooser);
 	    windo2.add(btn1);
 	    windo2.add(tf1);
-	    windo2.add(chooser);
 	    windo2.setLayout(new FlowLayout(FlowLayout.CENTER));
+	    
 	}
 	Label lblOne = new Label("Do Math Here");
-	public void changeLabel(String label) {
-		lblOne.setText(label);
-	}
 	public Frame windo2 = new Frame("Bram's Math Program");
 	public void close() {
 		windo2.dispose();
 	}
+	
 	Button btn1 = new Button("Compute");
 	Choice chooser = new Choice();
 	boolean isActive = false;
@@ -58,15 +58,16 @@ public class MathWindow implements ActionListener {
 			xandy.close();
 			String slopeForm = SlopeIntercept.calculate(x, x2, y, y2);
 			tf1.setText(slopeForm);
-			this.changeLabel("Slope-Intercept Form is");
-			windo2.setLayout(new FlowLayout(FlowLayout.CENTER));
-		} else if(chosenMath=="End Program") {
-			System.out.println("EndChosen");
-			this.stop();
-			this.close();
 		}
 	}
+	WindowAdapter l = new WindowAdapter() {
+		@Override
+		public void windowClosing(WindowEvent we) {
+			MathWindow.this.close();
+		}
+	};
 	public void start() throws InterruptedException {
 		btn1.addActionListener(this);
+		windo2.addWindowListener(l);
 	}
 }
