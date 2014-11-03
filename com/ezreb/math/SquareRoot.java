3cbf6f59bb1afb46c1a7bcfd3a4a0bd0b7068ea8
MrabEzreb <zerbebram@gmail.com>
2014-11-03 12:50:52 -0500

    leaving 2

--- com/ezreb/math/SquareRoot.java
+++ com/ezreb/math/SquareRoot.java
@@ -3,7 +3,8 @@ package com.ezreb.math;
 import java.util.Scanner;
 
 public class SquareRoot implements MathFunction {
-
+	public SquareRoot() {
+	}
 	@Override
 	public String calculate(double x1, double y1, double x2, double y2) {
 		PrimeFactorization PrimeFactorization = new PrimeFactorization();
@@ -11,9 +12,17 @@ public class SquareRoot implements MathFunction {
 		Scanner root = new Scanner(answer1);
 		root.useDelimiter(",");
 		int[] root2 = new int[50];
-		int current 
+		int currentFactor = 0;
 		while(root.hasNextInt()==true) {
-			root2[] = root.nextInt();
+			root2[currentFactor] = root.nextInt();
+			currentFactor = currentFactor+1;
+		}
+		for(int prime : PrimeFactorization.primes) {
+			for(int factor : root2) {
+				if(factor~=prime) {
+					
+				}
+			}
 		}
 		return null;
 	}
