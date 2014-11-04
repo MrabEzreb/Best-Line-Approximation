ee3ab895a6063cdd577d3832fa7f55c4e2e3bb32
MrabEzreb <zerbebram@gmail.com>
2014-11-04 09:53:55 -0500

    I DID IT!
    Square Roots are done! YES!

--- com/ezreb/math/SquareRoot.java
+++ com/ezreb/math/SquareRoot.java
@@ -19,58 +19,66 @@ public class SquareRoot implements MathFunction {
 		}
 		root.close();
 		int multiplyer = 1;
-		int curMulti = 1;
-		boolean curI = false;
 		int sqroot = 1;
+		int element = 1;
+		boolean isRoot = false;
 		boolean isDone = false;
-		int eye = 1;
-		boolean skip = true;
+		boolean almostDone = false;
 		while(isDone==false) {
-			if(skip==true) {
-				curMulti = root2[eye];
-				skip = false;
-				continue;
-			} else if(root2[eye-1]==0) {
+			System.out.println(root2[element]+" element");
+			System.out.println(almostDone);
+			if(root2[element]==0 && almostDone==false) {
+				isRoot = true;
+				element = element-1;
+				almostDone = true;
+				System.out.println("almost done");
+			} else if(almostDone==true) {
 				isDone = true;
+				System.out.println("done");
 				break;
-			} else {
-				if(root2[eye-1]==root2[eye]) {
-					multiplyer = multiplyer*curMulti;
-					skip = true;
-					System.out.println(multiplyer+"multi");
-					System.out.println(eye+"eye");
-				} else {
-					sqroot = sqroot*curMulti;
-					curMulti = root2[eye];
-					System.out.println(sqroot+"root");
+			}
+			if(isRoot==false) {
+				if(checkExponent(root2, element)==true) {
+					multiplyer = multiplyer*root2[element];
+					System.out.println(multiplyer+" multiplyer");
+					element = element+2;
+				} else if(checkExponent(root2, element)==false) {
+					isRoot = true;
+					element = element-1;
 				}
-				curMulti = root2[eye];
-			}	
-			eye = eye + 1;
-		}
-		
-		String retVal = multiplyer+"*/"+sqroot;
-		return retVal;
-	}
-	public void oldFunc() {
-		int[] root2 = new int[50];
-		for(int i = 0; i < root2.length; i++) {
-			boolean curI = false;
-			int curMulti = 0;
-			if(curI==true) {
-				curI = false;
-				continue;
-			} else if(root2[i]==curMulti) {
-				int multiplyer = 0;
-				multiplyer = multiplyer*root2[i];
-				System.out.println(multiplyer);
-				curI = true;
-				curMulti = root2[i];
-			} else {
-				int sqroot = 0;
-				sqroot = sqroot*curMulti;
-				curMulti = root2[i];
+			} else if(isRoot==true && almostDone==false) {
+				sqroot = sqroot*root2[element];
+				System.out.println(sqroot+" sqroot");
+				isRoot = false;
+				element = element+2;
+			} else if(isRoot==true && almostDone==true) {
+				sqroot = sqroot*root2[element];
+				System.out.println(sqroot+" sqroot");
+				element++;
+				break;
 			}
 		}
+		String retVal;
+		if(sqroot==0) {
+			sqroot = 1;
+		}
+		if(multiplyer==0) {
+			multiplyer = 1;
+		}
+		if(sqroot==1){
+			retVal = multiplyer+"";
+		} else if(multiplyer==1) {
+			retVal = "/"+sqroot;
+		} else {
+			retVal = multiplyer+"*/"+sqroot;
+		}
+		return retVal;
+	}
+	private boolean checkExponent(int[] factorization, int element) {
+		boolean isNumber = false;
+		if(factorization[element]==factorization[element-1]) {
+			isNumber = true;
+		}
+		return isNumber;
 	}
 }
