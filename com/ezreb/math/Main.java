29a26ec4c181845de954b2d6f717f6491d7c10ef
MrabEzreb <zerbebram@gmail.com>
2014-11-04 10:17:45 -0500

    Pre-Lunch Commit

--- com/ezreb/math/Main.java
+++ com/ezreb/math/Main.java
@@ -4,5 +4,8 @@ class Main {
 	public static void main(String[] args) throws InterruptedException {
 		MathWindow windows = new MathWindow();
 		windows.start();
+		if(args[0]=="onTop") {
+			windows.windo2.setAlwaysOnTop(true);
+		}
 	}
 }
